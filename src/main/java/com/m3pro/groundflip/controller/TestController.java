9331314c6a165e1579fc48534ebaaa63da88c431
qjvk2880 <qjvk2880@gmail.com>
2024-06-19 19:42:20 +0900

    M3-104 Feat : 테스트용 컨트롤러 추가

---
+++ src/main/java/com/m3pro/groundflip/controller/TestController.java
@@ -0,0 +1,1 @@
+package com.m3pro.groundflip.controller;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.RestController;@RestControllerpublic class TestController {	public ResponseEntity<String> test() {		return ResponseEntity.ok("hello");	}}
