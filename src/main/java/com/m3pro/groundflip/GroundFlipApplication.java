50247db2a0a9238c05703e18e3b1276ce1e0423d
qjvk2880 <qjvk2880@gmail.com>
2024-06-19 19:35:06 +0900

    M3-104 Feat : Initial Commit

---
+++ src/main/java/com/m3pro/groundflip/GroundFlipApplication.java
@@ -0,0 +1,13 @@
+package com.m3pro.groundflip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundFlipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroundFlipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m3pro/groundflip/GroundFlipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m3pro.groundflip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundFlipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
