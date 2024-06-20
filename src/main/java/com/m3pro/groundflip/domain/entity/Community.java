834324a9dbb1834931016747153f90ee5ab72f8b
qjvk2880 <qjvk2880@gmail.com>
2024-06-20 15:20:30 +0900

    M3-104 Feat : 기본 엔티티 생성

---
+++ src/main/java/com/m3pro/groundflip/domain/entity/Community.java
@@ -0,0 +1,36 @@
+package com.m3pro.groundflip.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "community")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Community {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "community_id")
+	private Long id;
+
+	private String name;
+
+	private String pixelColor;
+
+	private String profileImageUrl;
+
+	private String backgroundImageUrl;
+
+	private String description;
+}
