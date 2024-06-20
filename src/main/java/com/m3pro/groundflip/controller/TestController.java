285969059df803580ae64973eabe5f6b8c4d7c67
qjvk2880 <qjvk2880@gmail.com>
2024-06-20 16:11:55 +0900

    M3-104 Feat : 공통 응답 객체와 사용 예시에 상태 코드 추가

--- src/main/java/com/m3pro/groundflip/controller/TestController.java
+++ src/main/java/com/m3pro/groundflip/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.m3pro.groundflip.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m3pro.groundflip.domain.dto.Response;
@@ -9,6 +11,7 @@ import com.m3pro.groundflip.domain.dto.Response;
 public class TestController {
 
 	@GetMapping("/")
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	public Response<String> test() {
 		return Response.createSuccess("hello");
 	}
