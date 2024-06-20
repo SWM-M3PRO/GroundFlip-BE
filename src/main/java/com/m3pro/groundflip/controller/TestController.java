e5268e01ee6f97af2cef085f26048f9c189d2c62
qjvk2880 <qjvk2880@gmail.com>
2024-06-20 16:04:35 +0900

    M3-104 Feat : 공통 응답 객체와 사용 예시 추가

--- src/main/java/com/m3pro/groundflip/controller/TestController.java
+++ src/main/java/com/m3pro/groundflip/controller/TestController.java
@@ -1,14 +1,15 @@
 package com.m3pro.groundflip.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.m3pro.groundflip.domain.dto.Response;
 
 @RestController
 public class TestController {
 
 	@GetMapping("/")
-	public ResponseEntity<String> test() {
-		return ResponseEntity.ok("hello");
+	public Response<String> test() {
+		return Response.createSuccess("hello");
 	}
 }
