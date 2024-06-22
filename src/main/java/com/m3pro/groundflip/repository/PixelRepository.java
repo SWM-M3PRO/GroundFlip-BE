48596e40973fd7a3991e35394790a58173920a03
qjvk2880 <qjvk2880@gmail.com>
2024-06-22 15:22:29 +0900

    M3-117 Feat : 개인전 픽셀 가져오는 api 구현 완료

--- src/main/java/com/m3pro/groundflip/repository/PixelRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/PixelRepository.java
@@ -1,1 +1,17 @@
-package com.m3pro.groundflip.repository;import org.springframework.data.jpa.repository.JpaRepository;import com.m3pro.groundflip.domain.entity.Pixel;public interface PixelRepository extends JpaRepository<Pixel, Long> {}
+package com.m3pro.groundflip.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.m3pro.groundflip.domain.entity.Pixel;
+
+public interface PixelRepository extends JpaRepository<Pixel, Long> {
+	@Query(value = "select pixel from Pixel pixel "
+		+ "where pixel.x between :currentX - :xRange / 2 and :currentX + :xRange / 2 "
+		+ "and pixel.y between :currentY - :yRange / 2 and :currentY + :yRange / 2 ")
+	List<Pixel> findAllNearPixels(@Param("currentX") int currentX, @Param("currentY") int currentY,
+		@Param("xRange") int xRange, @Param("yRange") int yRange);
+}
