631c6531a9abfc58a22892d400e060b1d6a26d08
qjvk2880 <qjvk2880@gmail.com>
2024-06-22 17:47:49 +0900

    M3-117 Style : 개행문자를 LF로 변경

--- src/main/java/com/m3pro/groundflip/service/PixelService.java
+++ src/main/java/com/m3pro/groundflip/service/PixelService.java
@@ -1,1 +1,23 @@
-package com.m3pro.groundflip.service;import java.util.List;import org.springframework.stereotype.Service;import com.m3pro.groundflip.domain.dto.pixel.IndividualPixelResponse;import com.m3pro.groundflip.repository.PixelRepository;import lombok.RequiredArgsConstructor;@Service@RequiredArgsConstructorpublic class PixelService {	private final PixelRepository pixelRepository;	public List<IndividualPixelResponse> getNearIndividualPixels(int currentX, int currentY, int xRange, int yRange) {		return pixelRepository.findAllNearPixels(currentX, currentY, xRange, yRange)			.stream()			.map(IndividualPixelResponse::from)			.toList();	}}
+package com.m3pro.groundflip.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.m3pro.groundflip.domain.dto.pixel.IndividualPixelResponse;
+import com.m3pro.groundflip.repository.PixelRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PixelService {
+	private final PixelRepository pixelRepository;
+
+	public List<IndividualPixelResponse> getNearIndividualPixels(int currentX, int currentY, int xRange, int yRange) {
+		return pixelRepository.findAllNearPixels(currentX, currentY, xRange, yRange)
+			.stream()
+			.map(IndividualPixelResponse::from)
+			.toList();
+	}
+}
