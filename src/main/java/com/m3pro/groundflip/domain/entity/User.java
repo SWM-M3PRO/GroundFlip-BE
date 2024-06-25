1f4fd8dfec7c7f16ebd7bfc403e5af697287797c
qjvk2880 <qjvk2880@gmail.com>
2024-06-25 11:18:41 +0900

    M3-139 Refactor : 개행문자를 lf로 변경

--- src/main/java/com/m3pro/groundflip/domain/entity/User.java
+++ src/main/java/com/m3pro/groundflip/domain/entity/User.java
@@ -1,1 +1,48 @@
-package com.m3pro.groundflip.domain.entity;import java.util.Date;import com.m3pro.groundflip.domain.entity.global.BaseTimeEntity;import com.m3pro.groundflip.enums.Gender;import com.m3pro.groundflip.enums.Provider;import jakarta.persistence.Column;import jakarta.persistence.Entity;import jakarta.persistence.GeneratedValue;import jakarta.persistence.GenerationType;import jakarta.persistence.Id;import jakarta.persistence.Table;import jakarta.validation.constraints.Email;import jakarta.validation.constraints.Pattern;import lombok.AccessLevel;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Getter;import lombok.NoArgsConstructor;@Getter@Entity@Table(name = "user")@NoArgsConstructor(access = AccessLevel.PROTECTED)@AllArgsConstructor@Builderpublic class User extends BaseTimeEntity {	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)	@Column(name = "user_id")	private Long id;	@Pattern(regexp = "[가-힣a-zA-Z0-9]{3,10}", message = "닉네임은 한글, 영어, 숫자를 조합해 3글자 이상, 10글자 이하입니다.")	private String nickname;	private Date birthYear;	private Gender gender;	private Provider provider;	@Email	private String email;	private Date deletedAt;}
+package com.m3pro.groundflip.domain.entity;
+
+import java.util.Date;
+
+import com.m3pro.groundflip.domain.entity.global.BaseTimeEntity;
+import com.m3pro.groundflip.enums.Gender;
+import com.m3pro.groundflip.enums.Provider;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Pattern(regexp = "[가-힣a-zA-Z0-9]{3,10}", message = "닉네임은 한글, 영어, 숫자를 조합해 3글자 이상, 10글자 이하입니다.")
+	private String nickname;
+
+	private Date birthYear;
+
+	private Gender gender;
+
+	private Provider provider;
+
+	@Email
+	private String email;
+
+	private Date deletedAt;
+}
