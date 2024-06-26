8cdc9d0231b78379f47fa660b0e8220c51aa3360
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-06-26 16:58:26 +0900

    M3-129 Fat : 그룹 검색 컨트롤러 swagger 설정

--- src/main/java/com/m3pro/groundflip/controller/CommunityController.java
+++ src/main/java/com/m3pro/groundflip/controller/CommunityController.java
@@ -4,17 +4,15 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m3pro.groundflip.domain.dto.community.ListResponseCommunity;
 import com.m3pro.groundflip.domain.dto.community.ResponseGetGroup;
-import com.m3pro.groundflip.domain.entity.Community;
 import com.m3pro.groundflip.service.CommunityService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,12 +20,14 @@ import lombok.RequiredArgsConstructor;
 public class CommunityController {
 	private final CommunityService communityService;
 
-	@GetMapping("/api/groups")//그룹 검색
+	@Operation(summary = "그룹 검색", description = "그룹 이름 검색 키워드로 그룹들을 검색")
+	@Parameter(name = "searchKeyword", description = "그룹 검색 키워드", example = "홍익대")
+	@GetMapping("/api/groups")
 	public List<ListResponseCommunity> getCommunitys(@RequestParam String searchKeyword) {
 		return communityService.findCommunityByName(searchKeyword);
 	}
 
-	@GetMapping("/api/groups/{groupId}")//그룹 정보 api인데 아직 미완성
+	@GetMapping("/api/groups/{groupId}")
 	public ResponseGetGroup findGroupById(@PathVariable Long groupId) {
 		return communityService.findCommunityById(groupId);
 	}
--- src/main/java/com/m3pro/groundflip/service/CommunityService.java
+++ src/main/java/com/m3pro/groundflip/service/CommunityService.java
@@ -18,17 +18,14 @@ public class CommunityService {
 
 	private final CommunityRepository communityRepository;
 
-	//그룹 검색 api
-	public List<ListResponseCommunity> findCommunityByName(String name){
-		List<Community> community = communityRepository.findByNameLike("%"+name+"%");
+	public List<ListResponseCommunity> findCommunityByName(String name) {
+		List<Community> community = communityRepository.findByNameLike("%" + name + "%");
 		return community.stream().map(ListResponseCommunity::from).toList();
 	}
 
-	// 그룹 정보 get api인데 일단 검색만 하면 되는걸 깜빡하고 작성해버렸습니다.
-	// 랭킹, 멤버카운트는 더미로 0을 넣어놨습니다. 나중에 계산해서 값을 넣을 수 있을 듯 합니다.
-	public ResponseGetGroup findCommunityById(Long id){
+	public ResponseGetGroup findCommunityById(Long id) {
 		Community community = communityRepository.findById(id)
 			.orElseThrow(() -> new IllegalArgumentException("Community not found"));
-		return ResponseGetGroup.from(community, 0,0);
+		return ResponseGetGroup.from(community, 0, 0);
 	}
 }
