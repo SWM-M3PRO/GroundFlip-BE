ce40562b13b7adc48f53f07ea11f21bca7202ff4
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-06-26 16:26:25 +0900

    M3-129 Fat : 개행 문자 변경

--- src/main/java/com/m3pro/groundflip/repository/CommunityRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/CommunityRepository.java
@@ -1,1 +1,14 @@
-package com.m3pro.groundflip.repository;import java.util.List;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.data.jpa.repository.Query;import org.springframework.data.repository.query.Param;import com.m3pro.groundflip.domain.entity.Community;public interface CommunityRepository extends JpaRepository<Community, Long> {	List<Community> findByNameLike(String name);}
+package com.m3pro.groundflip.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.m3pro.groundflip.domain.entity.Community;
+
+public interface CommunityRepository extends JpaRepository<Community, Long> {
+
+	List<Community> findByNameLike(String name);
+}
