940b5122e1e76ebcb099373a959460cc9e032895
koomin1227 <koomin1227@naver.com>
2024-06-27 18:52:21 +0900

    M3-115 Feat : IndividualHistoryPixelResponse dto 생성

---
+++ src/main/java/com/m3pro/groundflip/domain/dto/pixel/IndividualHistoryPixelResponse.java
@@ -0,0 +1,29 @@
+package com.m3pro.groundflip.domain.dto.pixel;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Schema(title = "개인기록 픽셀 정보")
+public class IndividualHistoryPixelResponse {
+	@Schema(description = "픽셀 ID", example = "78611")
+	private long pixelId;
+
+	@Schema(description = "픽셀 좌측 상단 위도", example = "37.503717")
+	private double latitude;
+
+	@Schema(description = "픽셀 좌측 상단 경도", example = "127.044317")
+	private double longitude;
+
+	@Schema(description = "픽셀 세로 상대 좌표", example = "224")
+	private long x;
+
+	@Schema(description = "픽셀 가로 상대 좌표", example = "210")
+	private long y;
+}
