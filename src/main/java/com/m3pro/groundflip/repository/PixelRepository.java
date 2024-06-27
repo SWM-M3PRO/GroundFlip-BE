87482a86fe78c6ba97b32d594b00ef5acd865a66
koomin1227 <koomin1227@naver.com>
2024-06-27 23:44:40 +0900

    M3-115 Feat : findAllIndividualPixelsHistoryByCoordinate db에서 개인 기록 픽셀을 가져오는 jpa 쿼리 구현

--- src/main/java/com/m3pro/groundflip/repository/PixelRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/PixelRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import com.m3pro.groundflip.domain.entity.Pixel;
 
 public interface PixelRepository extends JpaRepository<Pixel, Long> {
+	@SuppressWarnings("checkstyle:RegexpSinglelineJava")
 	@Query(value = """
 		WITH PixelsInRange AS (
 		    SELECT
@@ -50,4 +51,31 @@ public interface PixelRepository extends JpaRepository<Pixel, Long> {
 		@Param("center") Point center,
 		@Param("radius") int radius);
 
+	@Query(value = """
+		WITH PixelsInRange AS (
+		    SELECT
+		        p.pixel_id,
+		        p.coordinate,
+		        p.x,
+		        p.y
+		    FROM
+		        pixel p
+		    WHERE
+		        ST_CONTAINS((ST_Buffer(:center, :radius)), p.coordinate)
+		)
+		 SELECT
+			distinct (pu.pixel_id),
+			pir.coordinate,
+			pir.x,
+		    pir.y
+		FROM
+			pixel_user pu
+		JOIN
+			PixelsInRange pir ON pu.pixel_id = pir.pixel_id
+		WHERE pu.user_id = :user_id
+		""", nativeQuery = true)
+	List<Object[]> findAllIndividualPixelsHistoryByCoordinate(
+		@Param("center") Point center,
+		@Param("radius") int radius,
+		@Param("user_id") Long userId);
 }
