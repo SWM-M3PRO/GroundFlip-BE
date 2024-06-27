a10d83f18b404e36d27593a068c11df7342407e3
koomin1227 <koomin1227@naver.com>
2024-06-27 18:56:38 +0900

    M3-115 Feat : getNearIndividualHistoryPixels 컨트롤러 구현

--- src/main/java/com/m3pro/groundflip/controller/PixelController.java
+++ src/main/java/com/m3pro/groundflip/controller/PixelController.java
@@ -1,5 +1,6 @@
 package com.m3pro.groundflip.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m3pro.groundflip.domain.dto.Response;
+import com.m3pro.groundflip.domain.dto.pixel.IndividualHistoryPixelResponse;
 import com.m3pro.groundflip.domain.dto.pixel.IndividualPixelResponse;
 import com.m3pro.groundflip.domain.dto.pixel.PixelOccupyRequest;
 import com.m3pro.groundflip.service.PixelService;
@@ -38,6 +40,20 @@ public class PixelController {
 		@RequestParam(name = "radius") int radius) {
 		return Response.createSuccess(
 			pixelService.getNearIndividualPixelsByCoordinate(currentLatitude, currentLongitude, radius));
+	}
+
+	@Operation(summary = "개인기록 픽셀 조회", description = "특정 좌표를 중심으로 반경 내 개인 기록 픽셀 정보를 조회 API")
+	@Parameters({
+		@Parameter(name = "current-latitude", description = "원의 중심 좌표의 위도", example = "37.503717"),
+		@Parameter(name = "current-longitude", description = "원의 중심 좌표의 경도", example = "127.044317"),
+		@Parameter(name = "radius", description = "미터 단위의 반경", example = "1000"),
+	})
+	@GetMapping("/individual-history")
+	public Response<List<IndividualHistoryPixelResponse>> getNearIndividualHistoryPixels(
+		@RequestParam(name = "current-latitude") double currentLatitude,
+		@RequestParam(name = "current-longitude") double currentLongitude,
+		@RequestParam(name = "radius") int radius) {
+		return Response.createSuccess(new ArrayList<>());
 	}
 
 	@Operation(summary = "픽셀 차지", description = "특정 픽셀의 id, 사용자 id, 커뮤니티 id를 사용해 소유권을 바꾸는 API ")
