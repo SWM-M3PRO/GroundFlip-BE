235f73b3b6143e09e225e6032a4224c5e1500143
koomin1227 <koomin1227@naver.com>
2024-06-28 13:13:05 +0900

    M3-115 Feat : 서버에러 ExcetionHandler 추가

--- src/main/java/com/m3pro/groundflip/exception/ErrorCode.java
+++ src/main/java/com/m3pro/groundflip/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 	DUPLICATED_USER(HttpStatus.BAD_REQUEST, "중복된 회원입니다."),
-	PIXEL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 픽셀입니다.");
+	PIXEL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 픽셀입니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러 입니다");
 
 	private final HttpStatus httpStatus;
 	private final String message;
