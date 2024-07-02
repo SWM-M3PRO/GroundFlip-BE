d4376c7577722c43b329964f4da3912f7e551ba4
koomin1227 <koomin1227@naver.com>
2024-07-02 16:31:28 +0900

    M3-143 Feat : 특정 픽셀에 방문한 정보를 쿼리하는 jpa 구현

--- src/main/java/com/m3pro/groundflip/repository/PixelUserRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/PixelUserRepository.java
@@ -1,8 +1,21 @@
 package com.m3pro.groundflip.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.m3pro.groundflip.domain.entity.PixelUser;
 
 public interface PixelUserRepository extends JpaRepository<PixelUser, Long> {
+	@Query(value = """
+		select pu.pixel_id, pu.user_id, u.nickname, u.profile_image from pixel_user pu
+		        join user u
+		          on pu.user_id = u.user_id
+		         where pu.pixel_id = :pixel_id
+		        group by pu.user_id;
+		""", nativeQuery = true)
+	List<Object[]> findAllVisitedUserByPixelId(
+		@Param("pixel_id") int pixelId);
 }
