64a05af769eef4f29bd022325afc8278621cf831
koomin1227 <koomin1227@naver.com>
2024-07-02 17:18:06 +0900

    M3-143 Feat : findAllVisitedUserByPixelId의 반환 dto 구현

--- src/main/java/com/m3pro/groundflip/repository/PixelUserRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/PixelUserRepository.java
@@ -6,17 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.m3pro.groundflip.domain.dto.pixelUser.VisitedUser;
 import com.m3pro.groundflip.domain.entity.PixelUser;
 
 public interface PixelUserRepository extends JpaRepository<PixelUser, Long> {
 	@Query(value = """
-		select pu.pixel_id, pu.user_id, u.nickname, u.profile_image from pixel_user pu
+		select pu.pixel_id as pixelId, pu.user_id as userId, u.nickname as nickname, u.profile_image as profileImage from pixel_user pu
 		        join user u
 		          on pu.user_id = u.user_id
 		         where pu.pixel_id = :pixel_id
 		        group by pu.user_id;
 		""", nativeQuery = true)
-	List<Object[]> findAllVisitedUserByPixelId(
+	List<VisitedUser> findAllVisitedUserByPixelId(
 		@Param("pixel_id") int pixelId);
 
 	@Query(value = """
