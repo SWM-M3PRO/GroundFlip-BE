0732cfae54ac2a9184d8954610817a6a785c4954
MinUk <qjvk2880@gmail.com>
2024-07-03 14:39:17 +0900

    M3-144 Style : native query 공백 및 개행 수정

--- src/main/java/com/m3pro/groundflip/repository/PixelUserRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/PixelUserRepository.java
@@ -15,48 +15,53 @@ import com.m3pro.groundflip.domain.entity.PixelUser;
 
 public interface PixelUserRepository extends JpaRepository<PixelUser, Long> {
 	@Query(value = """
-		select pu.pixel_id as pixelId, pu.user_id as userId, u.nickname as nickname, u.profile_image as profileImage from pixel_user pu
-		        join user u
-		          on pu.user_id = u.user_id 
-		         where pu.pixel_id = :pixel_id and pu.created_at >= current_date() 
-		        group by pu.user_id;
-		""", nativeQuery = true)
+			SELECT pu.pixel_id AS pixelId,
+			       pu.user_id AS userId,
+			       u.nickname AS nickname,
+			       u.profile_image AS profileImage 
+			FROM pixel_user pu
+			JOIN user u ON pu.user_id = u.user_id 
+			WHERE pu.pixel_id = :pixel_id AND pu.created_at >= current_date() 
+			GROUP BY pu.user_id;
+			""", nativeQuery = true)
 	List<VisitedUser> findAllVisitedUserByPixelId(
 		@Param("pixel_id") Long pixelId);
 
 	@Query(value = """
-		select pu.user_id as userId, u.nickname as nickname, u.profile_image as profileImage from pixel_user pu
-		        join user u
-		            on pu.user_id = u.user_id
-		        where pu.pixel_id = :pixel_id
-		        order by pu.created_at desc
-		        limit 1;
+		SELECT pu.user_id AS userId,
+		       u.nickname AS nickname,
+		       u.profile_image AS profileImage 
+		FROM pixel_user pu 
+		JOIN user u ON pu.user_id = u.user_id 
+		WHERE pu.pixel_id = :pixel_id 
+		ORDER BY pu.created_at DESC 
+		LIMIT 1
 		""", nativeQuery = true)
 	PixelOwnerUser findCurrentOwnerByPixelId(
 		@Param("pixel_id") Long pixelId);
 
 	@Query(value = """
-		SELECT COUNT(DISTINCT pu.pixel_id) as count
-		FROM pixel_user pu
-		WHERE pu.user_id = :user_id;
+		SELECT COUNT(DISTINCT pu.pixel_id) AS count 
+		FROM pixel_user pu 
+		WHERE pu.user_id = :user_id
 		""", nativeQuery = true)
 	PixelCount findAccumulatePixelCountByUserId(
 		@Param("user_id") Long userId);
 
 	@Query(value = """
-		SELECT count(user_id) as count
+		SELECT count(user_id) AS count 
 		FROM (SELECT pu.*
 		FROM pixel_user pu
 		         JOIN (
 		    SELECT pixel_id, MAX(created_at) AS latest_created_at
 		    FROM pixel_user
-		    WHERE pixel_id IN (SELECT DISTINCT pu.pixel_id as unique_pixel_count 
+		    WHERE pixel_id IN (SELECT DISTINCT pu.pixel_id AS unique_pixel_count 
 		                       FROM pixel_user pu WHERE pu.user_id = :user_id)
 		    GROUP BY pixel_id
-		) latest ON pu.pixel_id = latest.pixel_id AND pu.created_at = latest.latest_created_at
-		WHERE pu.pixel_id IN (SELECT DISTINCT pu.pixel_id as unique_pixel_count 
+		) latest ON pu.pixel_id = latest.pixel_id AND pu.created_at = latest.latest_created_at 
+		WHERE pu.pixel_id IN (SELECT DISTINCT pu.pixel_id AS unique_pixel_count  
 		                      FROM pixel_user pu WHERE pu.user_id = :user_id)) res
-		where res.user_id = :user_id
+		where res.user_id = :user_id 
 		""", nativeQuery = true)
 	PixelCount findCurrentPixelCountByUserId(
 		@Param("user_id") Long userId);
