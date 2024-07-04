d165d95ed99f7d8bdfa28d296b34ba0766a6d293
koomin1227 <koomin1227@naver.com>
2024-07-04 19:31:58 +0900

    M3-143 Style : 줄 구분 기호 변경

--- src/main/java/com/m3pro/groundflip/controller/UserController.java
+++ src/main/java/com/m3pro/groundflip/controller/UserController.java
@@ -1,1 +1,32 @@
-package com.m3pro.groundflip.controller;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PathVariable;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;import com.m3pro.groundflip.domain.dto.Response;import com.m3pro.groundflip.domain.dto.user.UserInfoResponse;import com.m3pro.groundflip.service.UserService;import io.swagger.v3.oas.annotations.Operation;import io.swagger.v3.oas.annotations.Parameter;import io.swagger.v3.oas.annotations.tags.Tag;import lombok.RequiredArgsConstructor;@RestController@RequiredArgsConstructor@RequestMapping("/api/users")@Tag(name = "users", description = "사용자 API")public class UserController {	private final UserService userService;	@Operation(summary = "사용자 기본 정보 조회", description = "닉네임, id, 출생년도, 성별, 프로필 사진, 그룹이름, 그룹 id 를 조회 한다.")	@GetMapping("/{userId}")	public Response<UserInfoResponse> getUserInfo(		@Parameter(description = "찾고자 하는 userId", required = true)		@PathVariable Long userId	) {		return Response.createSuccess(userService.getUserInfo(userId));	}}
+package com.m3pro.groundflip.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.m3pro.groundflip.domain.dto.Response;
+import com.m3pro.groundflip.domain.dto.user.UserInfoResponse;
+import com.m3pro.groundflip.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@Tag(name = "users", description = "사용자 API")
+public class UserController {
+	private final UserService userService;
+
+	@Operation(summary = "사용자 기본 정보 조회", description = "닉네임, id, 출생년도, 성별, 프로필 사진, 그룹이름, 그룹 id 를 조회 한다.")
+	@GetMapping("/{userId}")
+	public Response<UserInfoResponse> getUserInfo(
+		@Parameter(description = "찾고자 하는 userId", required = true)
+		@PathVariable Long userId
+	) {
+		return Response.createSuccess(userService.getUserInfo(userId));
+	}
+}
