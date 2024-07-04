7574755e5e79f17db1dceb940956a3c0c9c670e0
koomin1227 <koomin1227@naver.com>
2024-07-04 17:12:19 +0900

    M3-143 Feat : UserInfoResponse dto 구현

---
+++ src/main/java/com/m3pro/groundflip/domain/dto/user/UserInfoResponse.java
@@ -0,0 +1,39 @@
+package com.m3pro.groundflip.domain.dto.user;
+
+import java.util.Date;
+
+import com.m3pro.groundflip.enums.Gender;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Schema(title = "사용자 정보")
+public class UserInfoResponse {
+	@Schema(description = "사용자 ID", example = "1234")
+	private Long userId;
+
+	@Schema(description = "사용자 닉네임", example = "홍길동")
+	private String nickname;
+
+	@Schema(description = "사용자 프로필 사진 주소", example = "http://www.test.com")
+	private String profileImageUrl;
+
+	@Schema(description = "그룹 ID", example = "123")
+	private Long communityId;
+
+	@Schema(description = "그룹 이름", example = "홍익대학교")
+	private String communityName;
+
+	@Schema(description = "사용자 출생년도", example = "2000")
+	private Date birthYear;
+
+	@Schema(description = "사용자 성별", example = "남성")
+	private Gender gender;
+}
