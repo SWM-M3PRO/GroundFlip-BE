d16a491b5ea191f96a2f3a6d3e76a3c72d8c7463
MinUk <qjvk2880@gmail.com>
2024-07-10 11:40:39 +0900

    M3-162 Style : 개행문자 수정

--- src/main/java/com/m3pro/groundflip/repository/UserRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/UserRepository.java
@@ -1,1 +1,12 @@
-package com.m3pro.groundflip.repository;import com.m3pro.groundflip.enums.Provider;import org.springframework.data.jpa.repository.JpaRepository;import com.m3pro.groundflip.domain.entity.User;import java.util.Optional;public interface UserRepository extends JpaRepository<User, Long> {    Optional<User> findByProviderAndEmail(Provider provider, String email);}
+package com.m3pro.groundflip.repository;
+
+import com.m3pro.groundflip.enums.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.m3pro.groundflip.domain.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderAndEmail(Provider provider, String email);
+}
