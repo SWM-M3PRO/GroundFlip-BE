9465fcfab14cdb5d458b340aaef23eb71ca78b8e
MinUk <qjvk2880@gmail.com>
2024-07-10 11:44:20 +0900

    M3-162 Feat : 로그인에 대한 스웨거 추가

--- src/main/java/com/m3pro/groundflip/controller/AuthController.java
+++ src/main/java/com/m3pro/groundflip/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.m3pro.groundflip.domain.dto.auth.LoginRequest;
 import com.m3pro.groundflip.domain.dto.auth.LoginResponse;
 import com.m3pro.groundflip.enums.Provider;
 import com.m3pro.groundflip.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "카카오 로그인", description = "카카오에서 받은 액세스 토큰을 통해 회원가입 또는 로그인하는 API")
     @PostMapping("/kakao/login")
     public Response<LoginResponse> loginKaKao(@RequestBody LoginRequest loginRequest) {
         return Response.createSuccess(authService.login(Provider.KAKAO, loginRequest));
