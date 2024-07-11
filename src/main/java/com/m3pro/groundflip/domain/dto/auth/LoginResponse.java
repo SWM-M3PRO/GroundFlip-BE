d0a5013f7a255473495cb79c1e0aad767ef6d1c4
koomin1227 <koomin1227@naver.com>
2024-07-11 11:11:37 +0900

    M3-177 Refactor : Tokens 을 클래스로 분리하고 LoginResponse 에서 상속

--- src/main/java/com/m3pro/groundflip/domain/dto/auth/LoginResponse.java
+++ src/main/java/com/m3pro/groundflip/domain/dto/auth/LoginResponse.java
@@ -3,20 +3,21 @@ package com.m3pro.groundflip.domain.dto.auth;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Schema(title = "로그인 응답")
-public class LoginResponse {
-	@Schema(description = "서버에서 발급한 액세스 토큰")
-	private String accessToken;
-
-	@Schema(description = "서버에서 발급한 리프레쉬 토큰")
-	private String refreshToken;
-
+public class LoginResponse extends Tokens {
 	@Schema(description = "최초 로그인(회원가입)인지 여부", example = "true")
 	private Boolean isSignUp;
+
+	public LoginResponse(String accessToken, String refreshToken, Boolean isSignUp) {
+		super(accessToken, refreshToken);
+		this.isSignUp = isSignUp;
+	}
 }
 
