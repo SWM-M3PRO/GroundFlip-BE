386d36cac2891d4d0d86fbb3768574b1e07cafa8
koomin1227 <koomin1227@naver.com>
2024-07-13 10:32:54 +0900

    M3-165 Feat : 블랙리스트 토큰 엔티티 구현

---
+++ src/main/java/com/m3pro/groundflip/domain/entity/redis/BlacklistedToken.java
@@ -0,0 +1,25 @@
+package com.m3pro.groundflip.domain.entity.redis;
+
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+
+@RedisHash("token")
+@AllArgsConstructor
+public class BlacklistedToken {
+	@Id
+	private String id;
+
+	private String token;
+
+	@TimeToLive
+	private Long timeToLive;
+
+	public BlacklistedToken(String token, Long timeToLive) {
+		this.id = token;
+		this.token = "logout";
+		this.timeToLive = timeToLive;
+	}
+}
