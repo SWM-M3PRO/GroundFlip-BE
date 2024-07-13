20f06a570d5146f0b4b30c79e08e6d70a561051d
koomin1227 <koomin1227@naver.com>
2024-07-13 14:42:46 +0900

    M3-165 Refactor : BlacklistedToken 필드 이름 변경

--- src/main/java/com/m3pro/groundflip/domain/entity/redis/BlacklistedToken.java
+++ src/main/java/com/m3pro/groundflip/domain/entity/redis/BlacklistedToken.java
@@ -1,9 +1,9 @@
 package com.m3pro.groundflip.domain.entity.redis;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
