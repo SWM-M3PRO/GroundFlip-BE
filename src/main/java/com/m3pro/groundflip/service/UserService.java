cf14d5a7cb0662678339d183f33ee1ae94ecfb9b
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-07-16 16:48:47 +0900

    M3-172 Refactor : 파일 위치 변경, 환경변수 수정

--- src/main/java/com/m3pro/groundflip/service/UserService.java
+++ src/main/java/com/m3pro/groundflip/service/UserService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.m3pro.groundflip.aws.S3Uploader;
+import com.m3pro.groundflip.util.S3Uploader;
 import com.m3pro.groundflip.domain.dto.user.UserInfoRequest;
 import com.m3pro.groundflip.domain.dto.user.UserInfoResponse;
 import com.m3pro.groundflip.domain.entity.User;
