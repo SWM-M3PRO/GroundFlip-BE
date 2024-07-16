64cb9494add61a359f4719a15df60d220ce9a55e
Koo Min <koomin1227@naver.com>
2024-07-16 19:54:03 +0900

    M3-183 Feat : 30등까지의 userId와 점수를 내림차순으로 정렬해서 반환하는 getRankingsWithScore 구현

--- src/main/java/com/m3pro/groundflip/repository/RankingRedisRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/RankingRedisRepository.java
@@ -1,8 +1,14 @@
 package com.m3pro.groundflip.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
+
+import com.m3pro.groundflip.domain.dto.ranking.Ranking;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RankingRedisRepository {
 	private static final String RANKING_KEY = "current_pixel_ranking";
+	private static final int RANKING_START_INDEX = 0;
+	private static final int RANKING_END_INDEX = 29;
 	private final RedisTemplate<String, String> redisTemplate;
 
 	public void increaseScore(Long userId) {
@@ -25,5 +33,13 @@ public class RankingRedisRepository {
 	public void save(Long userId) {
 		ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
 		zSetOperations.add(RANKING_KEY, userId.toString(), 0);
+	}
+
+	public List<Ranking> getRankingsWithScore() {
+		ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+		return new ArrayList<>(
+			(Objects.requireNonNull(
+				zSetOperations.reverseRangeWithScores(RANKING_KEY, RANKING_START_INDEX, RANKING_END_INDEX))))
+			.stream().map(Ranking::from).toList();
 	}
 }
