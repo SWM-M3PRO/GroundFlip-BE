78a2168e105f3ae861781757a33aa8f7595079d6
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-07-16 16:03:39 +0900

    M3-172 Refactor : unused import 정리

--- src/main/java/com/m3pro/groundflip/controller/UserController.java
+++ src/main/java/com/m3pro/groundflip/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.m3pro.groundflip.controller;
 
-import java.io.IOException;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
