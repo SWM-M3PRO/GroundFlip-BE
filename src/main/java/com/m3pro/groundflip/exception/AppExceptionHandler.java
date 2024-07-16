216fa0a7e5ce5f4b182cde8c18dcc7a7b9b70970
MinUk <qjvk2880@gmail.com>
2024-07-16 16:31:29 +0900

    M3-190 Feat : 인터셉터를 통한 로그 구현 완료

--- src/main/java/com/m3pro/groundflip/exception/AppExceptionHandler.java
+++ src/main/java/com/m3pro/groundflip/exception/AppExceptionHandler.java
@@ -5,20 +5,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
+@Slf4j
 public class AppExceptionHandler {
+	private static final String LOG_ID = "logId";
+
 	@ExceptionHandler(AppException.class)
 	ResponseEntity<ErrorResponse> handleAppException(AppException appException, HttpServletRequest request) {
-		return ResponseEntity
-			.status(appException.getErrorCode().getHttpStatus())
+		log.info("EXCEPTION [{}] [{}] [{}] [{}]", request.getAttribute(LOG_ID), request.getRequestURI(),
+			request.getMethod(), appException.getErrorCode().getMessage());
+
+		return ResponseEntity.status(appException.getErrorCode().getHttpStatus())
 			.body(ErrorResponse.of(appException.getErrorCode()));
 	}
 
 	@ExceptionHandler(Exception.class)
 	ResponseEntity<ErrorResponse> handleException(Exception exception, HttpServletRequest request) {
-		return ResponseEntity
-			.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+		return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
 			.body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
 	}
 }
