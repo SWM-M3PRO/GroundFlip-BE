0ee3b1ac4da04e53b1b95a9e9ad4a685809d902b
MinUk <qjvk2880@gmail.com>
2024-07-17 12:30:09 +0900

    M3-190 Chore : timezone 관련 코드 추가

--- src/main/java/com/m3pro/groundflip/GroundFlipApplication.java
+++ src/main/java/com/m3pro/groundflip/GroundFlipApplication.java
@@ -1,15 +1,25 @@
 package com.m3pro.groundflip;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class GroundFlipApplication {
-	static{
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GroundFlipApplication.class, args);
 	}
