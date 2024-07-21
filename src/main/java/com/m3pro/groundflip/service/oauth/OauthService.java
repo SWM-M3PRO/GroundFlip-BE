b7ac59a527e4315607b62ff152b4a75f31f2d1f6
MinUk <qjvk2880@gmail.com>
2024-07-21 18:40:09 +0900

    M3-199 Chore : OauthService 공통 주석 추가

--- src/main/java/com/m3pro/groundflip/service/oauth/OauthService.java
+++ src/main/java/com/m3pro/groundflip/service/oauth/OauthService.java
@@ -22,6 +22,13 @@ public class OauthService {
 		);
 	}
 
+	/**
+	 * Oauth Provider로부터 사용자 정보를 가져온다.
+	 * @param provider oauth 프로바이더
+	 * @param accessToken 카카오의 액세스 토큰
+	 * @return OauthUserInfoResponse 프로바이더에서 받아온 사용자의 정보
+	 * @author 김민욱
+	 */
 	public OauthUserInfoResponse requestUserInfo(Provider provider, String accessToken) {
 		if (!isOauthTokenValid(provider, accessToken)) {
 			throw new AppException(ErrorCode.UNAUTHORIZED);
@@ -30,6 +37,13 @@ public class OauthService {
 		return oauthApiClient.requestOauthUserInfo(accessToken);
 	}
 
+	/**
+	 * Oauth Provider로부터 토큰을 검증한다.
+	 * @param provider oauth 프로바이더
+	 * @param accessToken 프로바이더의 액세스 토큰
+	 * @return boolean 토큰이 유효한지 여부
+	 * @author 김민욱
+	 */
 	private boolean isOauthTokenValid(Provider provider, String accessToken) {
 		OauthApiClient oauthApiClient = clients.get(provider);
 		return oauthApiClient.isOauthTokenValid(accessToken);
