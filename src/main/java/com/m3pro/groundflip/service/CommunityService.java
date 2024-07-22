a186a541de3e2f0b3f81131d082ac86622cc1e84
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-07-22 21:25:47 +0900

    M3-200 Feat : 주석 작성

--- src/main/java/com/m3pro/groundflip/service/CommunityService.java
+++ src/main/java/com/m3pro/groundflip/service/CommunityService.java
@@ -17,6 +17,11 @@ public class CommunityService {
 
 	private final CommunityRepository communityRepository;
 
+	/*
+	 * 그룹명을 검색한다
+	 * @param 그룹 name String
+	 * @return 해당 String이 포함된 모든 그룹 name List
+	 * */
 	public List<CommunitySearchResponse> findAllCommunityByName(String name) {
 		List<Community> community = communityRepository.findAllByNameLike("%" + name + "%");
 		return community.stream().map(CommunitySearchResponse::from).toList();
