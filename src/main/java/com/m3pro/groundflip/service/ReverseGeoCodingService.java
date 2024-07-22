cab02bcfcfe9e4dfcf0cda027031f3084ff2175f
Koo Min <koomin1227@naver.com>
2024-07-22 15:28:13 +0900

    M3-198 Chore : ReverseGeoCodingService 공통 주석 추가

--- src/main/java/com/m3pro/groundflip/service/ReverseGeoCodingService.java
+++ src/main/java/com/m3pro/groundflip/service/ReverseGeoCodingService.java
@@ -23,6 +23,12 @@ public class ReverseGeoCodingService {
 	@Value("${naver.apiKey}")
 	private String apiKey;
 
+	/**
+	 * 특정 좌표의 주소를 얻어온다.
+	 * @param longitude 경도
+	 * @param latitude 위도
+	 * @return 주소 if 대한민국 주소가 없으면 null 반환
+	 */
 	public String getAddressFromCoordinates(double longitude, double latitude) {
 		NaverReverseGeoCodingApiResult naverReverseGeoCodingApiResult =
 			fetchNaverReverseGeoCodingApiResult(longitude, latitude);
@@ -35,6 +41,12 @@ public class ReverseGeoCodingService {
 		}
 	}
 
+	/**
+	 * 네이버 리버스 지오 코딩 api 로 부터 주소 정보를 얻어온다.
+	 * @param longitude 경도
+	 * @param latitude 위도
+	 * @return 네이버 리버스 지오 코딩 api 응답 정보
+	 */
 	private NaverReverseGeoCodingApiResult fetchNaverReverseGeoCodingApiResult(double longitude, double latitude) {
 		String coordinate = String.format("%f, %f", longitude, latitude);
 		URI uri = UriComponentsBuilder.fromHttpUrl(NAVER_API_URL)
