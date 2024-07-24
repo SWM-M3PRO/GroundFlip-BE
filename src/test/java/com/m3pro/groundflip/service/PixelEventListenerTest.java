74d6c291dbe783fe8d3c99dff9e1c7f069c3b9a2
MinUk <qjvk2880@gmail.com>
2024-07-24 15:39:27 +0900

    M3-226 Feat : PixelEventListener에 대한 테스트 코드 추가

--- src/test/java/com/m3pro/groundflip/service/PixelEventListenerTest.java
+++ src/test/java/com/m3pro/groundflip/service/PixelEventListenerTest.java
@@ -1,48 +1,67 @@
 package com.m3pro.groundflip.service;
 
+import static org.mockito.Mockito.*;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.m3pro.groundflip.domain.dto.pixel.event.PixelAddressUpdateEvent;
 import com.m3pro.groundflip.domain.dto.pixel.event.PixelUserInsertEvent;
 import com.m3pro.groundflip.domain.entity.Pixel;
+import com.m3pro.groundflip.repository.PixelRepository;
+import com.m3pro.groundflip.repository.PixelUserRepository;
 
-import jakarta.transaction.Transactional;
-
-@SpringBootTest
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(MockitoExtension.class)
 public class PixelEventListenerTest {
-	@Autowired
-	private ApplicationContext applicationContext;
-	@MockBean
+	private static final int WGS84_SRID = 4326;
+
+	@Mock
+	private PixelUserRepository pixelUserRepository;
+	@Mock
+	private ReverseGeoCodingService reverseGeoCodingService;
+	@Mock
+	private PixelRepository pixelRepository;
+	@InjectMocks
 	private PixelEventListener pixelEventListener;
 
 	@Test
-	@Transactional
-	@DisplayName("[insertPixelUserHistory] 픽셀 방문 기록 이벤트가 정상적으로 수신되는지 확인")
-	void insertPixelUserEventListener() {
-		PixelUserInsertEvent event = new PixelUserInsertEvent(1L, 1L, null);
-		applicationContext.publishEvent(event);
+	@DisplayName("[insertPixelUserHistory] save 메소드가 정상적으로 실행되는지 확인")
+	public void insertPixelUserHistoryTest() {
+		pixelEventListener.insertPixelUserHistory(new PixelUserInsertEvent(1L, 1L, -1L));
 
-		Mockito.verify(pixelEventListener).insertPixelUserHistory(event);
+		verify(pixelUserRepository, times(1)).save(1L, 1L, -1L);
 	}
 
 	@Test
-	@Transactional
-	@DisplayName("[updatePixelAddress] 픽셀 주소 업데이트 이벤트가 정상적으로 수신되는지 확인")
-	void updatePixelAddressEventListener() {
-		PixelAddressUpdateEvent event = new PixelAddressUpdateEvent(Pixel.builder().build());
-		applicationContext.publishEvent(event);
+	@DisplayName("[updatePixelAddress] 픽셀 주소가 업데이트 되는 지 확인")
+	public void updatePixelAddressTest() {
+		Pixel pixel = Pixel.builder()
+			.coordinate(createPoint(127.0, 37.0))
+			.address(null)
+			.build();
 
-		Mockito.verify(pixelEventListener).updatePixelAddress(event);
+		when(reverseGeoCodingService.getAddressFromCoordinates(127.0, 37.0))
+			.thenReturn("서울시 은평구");
+
+		pixelEventListener.updatePixelAddress(new PixelAddressUpdateEvent(
+			pixel
+		));
+
+		Assertions.assertThat(pixel.getAddress()).isEqualTo("서울시 은평구");
 	}
 
+	private Point createPoint(double longitude, double latitude) {
+		GeometryFactory geometryFactory = new GeometryFactory();
+		Point point = geometryFactory.createPoint(new Coordinate(longitude, latitude));
+		point.setSRID(WGS84_SRID);
+		return point;
+	}
 }
