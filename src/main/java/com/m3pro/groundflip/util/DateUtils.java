82d2d172fea82527171217e6eb60017c26d15392
Koo Min <koomin1227@naver.com>
2024-07-30 11:18:57 +0900

    M3-224 Feat : 이번주의 월요일에 해당하는 날짜를 반환하는 함수 구현

--- src/main/java/com/m3pro/groundflip/util/DateUtils.java
+++ src/main/java/com/m3pro/groundflip/util/DateUtils.java
@@ -2,6 +2,7 @@ package com.m3pro.groundflip.util;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.WeekFields;
 
 public class DateUtils {
@@ -12,5 +13,10 @@ public class DateUtils {
 	public static int getWeekOfDate(LocalDate date) {
 		WeekFields weekFields = WeekFields.of(DayOfWeek.MONDAY, 1);
 		return date.get(weekFields.weekOfWeekBasedYear());
+	}
+
+	public static LocalDate getThisWeekStartDate() {
+		LocalDate today = LocalDate.now();
+		return today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
 	}
 }
