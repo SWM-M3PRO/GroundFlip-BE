b8ad42d78aa21777412a45102baeb74d059abd57
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-08-06 16:33:58 +0900

    M3-212 Feat : 즐겨찾기 entity, dto 작성

---
+++ src/main/java/com/m3pro/groundflip/domain/dto/user/UserPlaceMarkRequest.java
@@ -0,0 +1,25 @@
+package com.m3pro.groundflip.domain.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Schema(title = "사용자 즐겨찾기 정보 등록")
+public class UserPlaceMarkRequest {
+
+	@Schema(description = "즐겨찾기 장소 이름", example = "학교")
+	private String placeName;
+
+	@Schema(description = "위도", example = "37.321147")
+	private double latitude;
+
+	@Schema(description = "경도", example = "127.093171")
+	private double longitude;
+
+}
