ea9f433b2b1433191ebc7ffd301c829dd5965590
Koo Min <koomin1227@naver.com>
2024-08-08 17:20:33 +0900

    M3-286 Feat : 회원 탈퇴시 fcm 토큰도 같이 삭제 하도록 수정

--- src/main/java/com/m3pro/groundflip/repository/FcmTokenRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/FcmTokenRepository.java
@@ -9,4 +9,6 @@ import com.m3pro.groundflip.domain.entity.User;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 	Optional<FcmToken> findByUser(User user);
+
+	void deleteByUser(User user);
 }
