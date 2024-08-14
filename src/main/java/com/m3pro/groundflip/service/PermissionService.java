f049d0a844baf96545c37d49494d28f789457388
Koo Min <koomin1227@naver.com>
2024-08-14 13:42:50 +0900

    M3-293 Feat : 특정 유저의 권한을 반환하는 메서드 구현

--- src/main/java/com/m3pro/groundflip/service/PermissionService.java
+++ src/main/java/com/m3pro/groundflip/service/PermissionService.java
@@ -3,6 +3,7 @@ package com.m3pro.groundflip.service;
 import org.springframework.stereotype.Service;
 
 import com.m3pro.groundflip.domain.dto.permission.PermissionRequest;
+import com.m3pro.groundflip.domain.dto.permission.PermissionResponse;
 import com.m3pro.groundflip.domain.entity.Permission;
 import com.m3pro.groundflip.domain.entity.User;
 import com.m3pro.groundflip.exception.AppException;
@@ -20,6 +21,16 @@ import lombok.extern.slf4j.Slf4j;
 public class PermissionService {
 	private final UserRepository userRepository;
 	private final PermissionRepository permissionRepository;
+
+	public PermissionResponse getAllPermissions(Long userId) {
+		User user = userRepository.getReferenceById(userId);
+		Permission permission = permissionRepository.findByUser(user)
+			.orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+		return new PermissionResponse(
+			permission.getServiceNotificationsEnabled(),
+			permission.getMarketingNotificationsEnabled()
+		);
+	}
 
 	@Transactional
 	public void updateServiceNotificationsPreference(PermissionRequest permissionRequest) {
