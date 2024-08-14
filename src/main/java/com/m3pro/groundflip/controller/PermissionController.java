5733e05d4180dfc9850ae786bc67fd88f745081b
Koo Min <koomin1227@naver.com>
2024-08-14 13:43:06 +0900

    M3-293 Feat : 유저의 권한을 반환하는 api 구현

--- src/main/java/com/m3pro/groundflip/controller/PermissionController.java
+++ src/main/java/com/m3pro/groundflip/controller/PermissionController.java
@@ -1,5 +1,7 @@
 package com.m3pro.groundflip.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.m3pro.groundflip.domain.dto.Response;
 import com.m3pro.groundflip.domain.dto.permission.PermissionRequest;
+import com.m3pro.groundflip.domain.dto.permission.PermissionResponse;
 import com.m3pro.groundflip.service.PermissionService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,6 +27,12 @@ import lombok.extern.slf4j.Slf4j;
 public class PermissionController {
 	private final PermissionService permissionService;
 
+	@Operation(summary = "유저가 동의한 권한 조회", description = "유저가 동의한 권한을 조회하여 응답한다.")
+	@GetMapping("/{userId}")
+	public Response<PermissionResponse> getPermission(@PathVariable("userId") Long userId) {
+		return Response.createSuccess(permissionService.getAllPermissions(userId));
+	}
+
 	@Operation(summary = "서비스 알림 권한 동의", description = "서비스 푸시 알림을 받을지 받지 않을지 선택하는 api 이다.")
 	@PutMapping("/service-notification")
 	public Response<?> updateServiceNotification(@RequestBody PermissionRequest permissionRequest) {
@@ -37,4 +46,5 @@ public class PermissionController {
 		permissionService.updateMarketingNotificationsPreference(permissionRequest);
 		return Response.createSuccessWithNoData();
 	}
+
 }
