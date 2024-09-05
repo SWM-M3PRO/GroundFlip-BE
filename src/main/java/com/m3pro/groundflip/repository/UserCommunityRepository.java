1293cf4e5d26645415cccc4c38fb8c67d2112123
Koo Min <koomin1227@naver.com>
2024-09-05 18:11:47 +0900

    M3-344 Feat : 그룹의 멤버수를 세는 메서드 구현

--- src/main/java/com/m3pro/groundflip/repository/UserCommunityRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/UserCommunityRepository.java
@@ -11,4 +11,7 @@ import com.m3pro.groundflip.domain.entity.UserCommunity;
 public interface UserCommunityRepository extends JpaRepository<UserCommunity, Long> {
 	@Query("SELECT uc FROM UserCommunity uc WHERE uc.user.id = :userId AND uc.deletedAt IS NULL")
 	List<UserCommunity> findByUserId(@Param("userId") Long userId);
+
+	@Query("SELECT COUNT(uc) FROM UserCommunity uc WHERE uc.community.id = :communityId AND uc.deletedAt IS NULL")
+	Long countByCommunityId(@Param("communityId") Long communityId);
 }
