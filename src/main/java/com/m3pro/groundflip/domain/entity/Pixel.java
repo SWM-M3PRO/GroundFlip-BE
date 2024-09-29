a5e98b76b051c0a78c0a538f6bf328a960329460
Koo Min <koomin1227@naver.com>
2024-09-29 18:18:55 +0900

    M3-378 Feat : Pixel의 auto increment 제거

--- src/main/java/com/m3pro/groundflip/domain/entity/Pixel.java
+++ src/main/java/com/m3pro/groundflip/domain/entity/Pixel.java
@@ -6,8 +6,6 @@ import org.locationtech.jts.geom.Point;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -22,7 +20,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Pixel {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "pixel_id")
 	private Long id;
 
