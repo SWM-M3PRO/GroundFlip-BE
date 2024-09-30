be8f3300b0de915a897e9728dab641cd0dff36b5
Koo Min <koomin1227@naver.com>
2024-10-01 03:17:27 +0900

    M3-382 Feat : 반경에 속한 지역 조회 쿼리 작성

---
+++ src/main/java/com/m3pro/groundflip/domain/dto/pixel/RegionInfo.java
@@ -0,0 +1,11 @@
+package com.m3pro.groundflip.domain.dto.pixel;
+
+public interface RegionInfo {
+	Long getRegionId();
+
+	double getLatitude();
+
+	double getLongitude();
+
+	String getName();
+}
--- src/main/java/com/m3pro/groundflip/repository/RegionRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/RegionRepository.java
@@ -1,8 +1,32 @@
 package com.m3pro.groundflip.repository;
 
+import java.util.List;
+
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import com.m3pro.groundflip.domain.entity.Pixel;
+import com.m3pro.groundflip.domain.dto.pixel.RegionInfo;
+import com.m3pro.groundflip.domain.entity.Region;
+import com.m3pro.groundflip.enums.RegionLevel;
 
-public interface RegionRepository extends JpaRepository<Pixel, Long> {
+public interface RegionRepository extends JpaRepository<Region, Long> {
+	@Query(value = """
+			SELECT
+				r.region_id,
+				ST_LATITUDE(r.coordinate) AS latitude,
+				ST_LONGITUDE(r.coordinate) AS longitude,
+				r.name
+			FROM
+				region r
+			WHERE
+				ST_CONTAINS((ST_Buffer(:center, :radius)), r.coordinate)
+			AND r.region_level = :region_level
+		""", nativeQuery = true)
+	List<RegionInfo> findAllCityRegionsByCoordinate(
+		@Param("center") Point center,
+		@Param("radius") int radius,
+		@Param("region_level") RegionLevel regionLevel
+	);
 }
