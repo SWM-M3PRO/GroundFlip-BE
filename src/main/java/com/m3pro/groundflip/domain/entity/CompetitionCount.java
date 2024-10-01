8fa0859ebcd33767f03ac56349e2d6b0a3fb01a5
Koo Min <koomin1227@naver.com>
2024-10-01 17:23:24 +0900

    M3-382 Feat : 경쟁 모드에서 집계 개수를 담을 CompetitionCount 생성

---
+++ src/main/java/com/m3pro/groundflip/domain/entity/CompetitionCount.java
@@ -0,0 +1,39 @@
+package com.m3pro.groundflip.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class CompetitionCount {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "competition_count_id")
+	private Long id;
+
+	private Integer individualModeCount;
+
+	private Integer communityModeCount;
+
+	private Integer year;
+
+	private Integer week;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "region_id")
+	private Region region;
+}
