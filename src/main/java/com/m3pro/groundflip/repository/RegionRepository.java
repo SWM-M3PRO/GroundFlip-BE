565cb5324c8e9bdd91cd5c4cb0eed2417ec2c789
Koo Min <koomin1227@naver.com>
2024-10-01 20:40:54 +0900

    M3-382 Feat : 지역 별 픽셀 개수 조회 쿼리 수정

--- src/main/java/com/m3pro/groundflip/repository/RegionRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/RegionRepository.java
@@ -16,16 +16,49 @@ public interface RegionRepository extends JpaRepository<Region, Long> {
 				r.region_id,
 				ST_LATITUDE(r.coordinate) AS latitude,
 				ST_LONGITUDE(r.coordinate) AS longitude,
-				r.name
+				r.name,
+				SUM(cc.individual_mode_count) AS count
 			FROM
 				region r
+			JOIN competition_count cc
+			ON r.region_id = cc.region_id
 			WHERE
 				ST_CONTAINS((ST_Buffer(:center, :radius)), r.coordinate)
-				AND r.region_level = :region_level
+				AND r.region_level = 'city'
+				AND cc.week = :week
+				AND cc.year = :year
+			GROUP BY r.region_id
 		""", nativeQuery = true)
 	List<RegionInfo> findAllCityRegionsByCoordinate(
 		@Param("center") Point center,
 		@Param("radius") int radius,
-		@Param("region_level") String regionLevel
+		@Param("week") int week,
+		@Param("year") int year
+	);
+
+	@Query(value = """
+			SELECT
+				p.region_id,
+				ST_LATITUDE(p.coordinate) AS latitude,
+				ST_LONGITUDE(p.coordinate) AS longitude,
+				p.name,
+				SUM(cc.individual_mode_count) AS count
+			FROM
+				region p
+			JOIN region r
+			ON p.region_id = r.parent_id
+			JOIN competition_count cc
+			ON r.region_id = cc.region_id
+			WHERE
+				p.region_level = 'province'
+				AND cc.week = :week
+				AND cc.year = :year
+				AND cc.individual_mode_count > 0
+			GROUP BY
+				p.region_id
+		""", nativeQuery = true)
+	List<RegionInfo> findAllProvinceRegionsByCoordinate(
+		@Param("week") int week,
+		@Param("year") int year
 	);
 }
