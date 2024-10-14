85bb59752cf4611f5440eb4daa39ecb3725f8cf7
Koo Min <koomin1227@naver.com>
2024-10-14 15:50:44 +0900

    M3-408 Feat : 현재 진행중인 이벤트 정보를 조회하는 쿼리 메소드 구현

--- src/main/java/com/m3pro/groundflip/repository/EventRepository.java
+++ src/main/java/com/m3pro/groundflip/repository/EventRepository.java
@@ -1,8 +1,15 @@
 package com.m3pro.groundflip.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.m3pro.groundflip.domain.entity.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+	@Query("SELECT e FROM Event e WHERE e.startDate <= :today AND e.endDate >= :today")
+	List<Event> findCurrentEvents(@Param("today") LocalDateTime today);
 }
