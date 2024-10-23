9f930c17589ecaff9f9823a8be88b3b1cbd1410a
Koo Min <koomin1227@naver.com>
2024-10-23 18:34:19 +0900

    M3-412 Feat : pixelManager 테스트 코드 수정

--- src/test/java/com/m3pro/groundflip/service/PixelManagerTest.java
+++ src/test/java/com/m3pro/groundflip/service/PixelManagerTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,20 +16,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.redisson.api.RFuture;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.context.ApplicationEventPublisher;
 
 import com.m3pro.groundflip.domain.dto.pixel.PixelOccupyRequest;
 import com.m3pro.groundflip.domain.dto.pixel.event.PixelAddressUpdateEvent;
-import com.m3pro.groundflip.domain.dto.pixel.event.PixelUserInsertEvent;
 import com.m3pro.groundflip.domain.dto.pixel.naverApi.ReverseGeocodingResult;
 import com.m3pro.groundflip.domain.entity.Pixel;
 import com.m3pro.groundflip.exception.AppException;
 import com.m3pro.groundflip.exception.ErrorCode;
+import com.m3pro.groundflip.repository.CommunityRepository;
 import com.m3pro.groundflip.repository.PixelRepository;
 import com.m3pro.groundflip.repository.PixelUserRepository;
+import com.m3pro.groundflip.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 class PixelManagerTest {
@@ -40,15 +36,16 @@ class PixelManagerTest {
 	private static final double upper_left_lat = 38.240675;
 	private static final double upper_left_lon = 125.905952;
 	private static final int WGS84_SRID = 4326;
-
+	@Mock
+	private UserRepository userRepository;
+	@Mock
+	private CommunityRepository communityRepository;
 	@Mock
 	private PixelRepository pixelRepository;
 	@Mock
 	private PixelUserRepository pixelUserRepository;
 	@Mock
 	private ApplicationEventPublisher applicationEventPublisher;
-	@Mock
-	private RedissonClient redissonClient;
 	@Mock
 	private UserRankingService userRankingService;
 	@Mock
@@ -72,10 +69,9 @@ class PixelManagerTest {
 			.address("대한민국")
 			.build();
 		when(pixelRepository.findByXAndY(222L, 233L)).thenReturn(Optional.of(pixel));
-		when(redissonClient.getLock(any())).thenReturn(new RedissonLock());
 		when(pixelUserRepository.existsByPixelIdAndUserId(any(), any())).thenReturn(false);
 		// When
-		pixelManager.occupyPixelWithLock(pixelOccupyRequest);
+		pixelManager.occupyPixel(pixelOccupyRequest);
 
 		//Then
 		verify(userRankingService, times(1)).updateCurrentPixelRanking(any(), any());
@@ -95,14 +91,13 @@ class PixelManagerTest {
 			.address("대한민국")
 			.build();
 		when(pixelRepository.findByXAndY(222L, 233L)).thenReturn(Optional.of(pixel));
-		when(redissonClient.getLock(any())).thenReturn(new RedissonLock());
 		// When
-		pixelManager.occupyPixelWithLock(pixelOccupyRequest);
+		pixelManager.occupyPixel(pixelOccupyRequest);
 
 		// Then
-		verify(applicationEventPublisher, times(1)).publishEvent(any(PixelUserInsertEvent.class));
 		verify(userRankingService, times(1)).updateAccumulatedRanking(any());
 		verify(communityRankingService, times(1)).updateCurrentPixelRanking(any(), any());
+		verify(pixelUserRepository, times(1)).save(any());
 	}
 
 	@Test
@@ -116,15 +111,13 @@ class PixelManagerTest {
 			.address(null)
 			.build();
 		when(pixelRepository.findByXAndY(222L, 233L)).thenReturn(Optional.of(pixel));
-		when(redissonClient.getLock(any())).thenReturn(new RedissonLock());
 		lenient().when(reverseGeoCodingService.getRegionFromCoordinates(Mockito.any(Double.class),
 			Mockito.any(Double.class))).thenReturn(
 			ReverseGeocodingResult.builder().regionId(null).regionName(null).build());
 		// When
-		pixelManager.occupyPixelWithLock(pixelOccupyRequest);
+		pixelManager.occupyPixel(pixelOccupyRequest);
 
 		// Then
-		// verify(applicationEventPublisher, times(1)).publishEvent(any(PixelAddressUpdateEvent.class));
 		verify(userRankingService, times(1)).updateAccumulatedRanking(any());
 		verify(communityRankingService, times(1)).updateCurrentPixelRanking(any(), any());
 	}
@@ -140,9 +133,8 @@ class PixelManagerTest {
 			.address("대한민국 ")
 			.build();
 		when(pixelRepository.findByXAndY(222L, 233L)).thenReturn(Optional.of(pixel));
-		when(redissonClient.getLock(any())).thenReturn(new RedissonLock());
 		// When
-		pixelManager.occupyPixelWithLock(pixelOccupyRequest);
+		pixelManager.occupyPixel(pixelOccupyRequest);
 
 		// Then
 		verify(applicationEventPublisher, times(0)).publishEvent(any(PixelAddressUpdateEvent.class));
@@ -154,11 +146,10 @@ class PixelManagerTest {
 	@DisplayName("[occupyPixel] 대한민국에 속한 pixel 이 아니라면 에러 발생")
 	void pixelIncorrectPixel() {
 		PixelOccupyRequest pixelOccupyRequest = new PixelOccupyRequest(5L, 78611L, 9000L, 233L);
-		when(redissonClient.getLock(any())).thenReturn(new RedissonLock());
 
 		// Then
 		AppException exception = assertThrows(AppException.class,
-			() -> pixelManager.occupyPixelWithLock(pixelOccupyRequest));
+			() -> pixelManager.occupyPixel(pixelOccupyRequest));
 		assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.PIXEL_NOT_FOUND);
 	}
 
@@ -172,7 +163,6 @@ class PixelManagerTest {
 			.userId(1L)
 			.address("대한민국 ")
 			.build();
-		when(redissonClient.getLock(any())).thenReturn(new RedissonLock());
 		double expectedLongitude = upper_left_lon + (233L * lon_per_pixel);
 		double expectedLatitude = upper_left_lat - (213L * lat_per_pixel);
 
@@ -180,173 +170,15 @@ class PixelManagerTest {
 		Point mockPoint = new GeometryFactory().createPoint(coordinate);
 		mockPoint.setSRID(WGS84_SRID);
 
-		// When - geometryFactory.createPoint를 모킹
 		when(geometryFactory.createPoint(any(Coordinate.class))).thenReturn(mockPoint);
 		when(pixelRepository.save(any())).thenReturn(pixel);
 		lenient().when(reverseGeoCodingService.getRegionFromCoordinates(Mockito.any(Double.class),
 			Mockito.any(Double.class))).thenReturn(
 			ReverseGeocodingResult.builder().regionId(null).regionName(null).build());
 		// When
-		pixelManager.occupyPixelWithLock(pixelOccupyRequest);
+		pixelManager.occupyPixel(pixelOccupyRequest);
 
 		// Then
 		verify(pixelRepository, times(1)).save(any());
-	}
-
-	static class RedissonLock implements RLock {
-		@Override
-		public String getName() {
-			return "";
-		}
-
-		@Override
-		public void lockInterruptibly(long l, TimeUnit timeUnit) throws InterruptedException {
-
-		}
-
-		@Override
-		public boolean tryLock(long l, long l1, TimeUnit timeUnit) throws InterruptedException {
-			return true;
-		}
-
-		@Override
-		public void lock(long l, TimeUnit timeUnit) {
-
-		}
-
-		@Override
-		public boolean forceUnlock() {
-			return false;
-		}
-
-		@Override
-		public boolean isLocked() {
-			return false;
-		}
-
-		@Override
-		public boolean isHeldByThread(long l) {
-			return false;
-		}
-
-		@Override
-		public boolean isHeldByCurrentThread() {
-			return false;
-		}
-
-		@Override
-		public int getHoldCount() {
-			return 0;
-		}
-
-		@Override
-		public long remainTimeToLive() {
-			return 0;
-		}
-
-		@Override
-		public void lock() {
-
-		}
-
-		@Override
-		public void lockInterruptibly() throws InterruptedException {
-
-		}
-
-		@Override
-		public boolean tryLock() {
-			return false;
-		}
-
-		@Override
-		public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-			return false;
-		}
-
-		@Override
-		public void unlock() {
-
-		}
-
-		@Override
-		public Condition newCondition() {
-			return null;
-		}
-
-		@Override
-		public RFuture<Boolean> forceUnlockAsync() {
-			return null;
-		}
-
-		@Override
-		public RFuture<Void> unlockAsync() {
-			return null;
-		}
-
-		@Override
-		public RFuture<Void> unlockAsync(long l) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Boolean> tryLockAsync() {
-			return null;
-		}
-
-		@Override
-		public RFuture<Void> lockAsync() {
-			return null;
-		}
-
-		@Override
-		public RFuture<Void> lockAsync(long l) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Void> lockAsync(long l, TimeUnit timeUnit) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Void> lockAsync(long l, TimeUnit timeUnit, long l1) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Boolean> tryLockAsync(long l) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Boolean> tryLockAsync(long l, TimeUnit timeUnit) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Boolean> tryLockAsync(long l, long l1, TimeUnit timeUnit) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Boolean> tryLockAsync(long l, long l1, TimeUnit timeUnit, long l2) {
-			return null;
-		}
-
-		@Override
-		public RFuture<Integer> getHoldCountAsync() {
-			return null;
-		}
-
-		@Override
-		public RFuture<Boolean> isLockedAsync() {
-			return null;
-		}
-
-		@Override
-		public RFuture<Long> remainTimeToLiveAsync() {
-			return null;
-		}
 	}
 }
