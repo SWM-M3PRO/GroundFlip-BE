1cdf5be84c012d83a1c46bc1a9eff391a7018214
Koo Min <koomin1227@naver.com>
2024-10-28 22:11:23 +0900

    M3-413 Feat : 업적 세부 정보를 반환하는 api 구현

--- src/main/java/com/m3pro/groundflip/controller/AchievementController.java
+++ src/main/java/com/m3pro/groundflip/controller/AchievementController.java
@@ -1,11 +1,13 @@
 package com.m3pro.groundflip.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m3pro.groundflip.domain.dto.Response;
+import com.m3pro.groundflip.domain.dto.achievement.AchievementResponse;
 import com.m3pro.groundflip.domain.dto.achievement.UserAchievementsResponse;
 import com.m3pro.groundflip.service.AchievementService;
 
@@ -37,5 +39,18 @@ public class AchievementController {
 		@RequestParam(name = "count", required = false) Integer count
 	) {
 		return Response.createSuccess(achievementService.getUserAchievements(userId, count));
+	}
+
+	@Operation(summary = "업적 정보 조회", description = "업적 정보를 조회하고 사용자의 업적 획득 상태를 반환한다.")
+	@Parameters({
+		@Parameter(name = "user-id", description = "찾고자 하는 user의 id", example = "14"),
+	})
+	@GetMapping("/{achievementId}")
+	public Response<AchievementResponse> getAchievementInfo(
+		@RequestParam(name = "user-id") Long userId,
+		@Parameter(description = "찾고자 하는 userId", required = true)
+		@PathVariable("achievementId") Long achievementId
+	) {
+		return Response.createSuccess(achievementService.getAchievement(achievementId, userId));
 	}
 }
