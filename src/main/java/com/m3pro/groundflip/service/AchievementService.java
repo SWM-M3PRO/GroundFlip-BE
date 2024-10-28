b0e1fd080105a71ee964e58dda1d11bfb5209e0e
Koo Min <koomin1227@naver.com>
2024-10-28 22:11:09 +0900

    M3-413 Feat : 업적 세부 정보를 조회하는 메서드 구현

--- src/main/java/com/m3pro/groundflip/service/AchievementService.java
+++ src/main/java/com/m3pro/groundflip/service/AchievementService.java
@@ -1,12 +1,17 @@
 package com.m3pro.groundflip.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.m3pro.groundflip.domain.dto.achievement.AchievementResponse;
 import com.m3pro.groundflip.domain.dto.achievement.UserAchievementsResponse;
+import com.m3pro.groundflip.domain.entity.Achievement;
 import com.m3pro.groundflip.domain.entity.UserAchievement;
+import com.m3pro.groundflip.exception.AppException;
+import com.m3pro.groundflip.exception.ErrorCode;
 import com.m3pro.groundflip.repository.AchievementCategoryRepository;
 import com.m3pro.groundflip.repository.AchievementRepository;
 import com.m3pro.groundflip.repository.UserAchievementRepository;
@@ -23,7 +28,7 @@ public class AchievementService {
 	private final UserAchievementRepository userAchievementRepository;
 
 	public UserAchievementsResponse getUserAchievements(Long userId, Integer count) {
-		Integer achievementCount = userAchievementRepository.countByUserId(userId);
+		Long achievementCount = userAchievementRepository.countByUserId(userId);
 		List<UserAchievement> userAchievements;
 		if (count == null) {
 			userAchievements = userAchievementRepository.findAllByUserId(userId);
@@ -31,5 +36,16 @@ public class AchievementService {
 			userAchievements = userAchievementRepository.findAllByUserId(userId, PageRequest.of(0, count));
 		}
 		return UserAchievementsResponse.from(userAchievements, achievementCount);
+	}
+
+	public AchievementResponse getAchievement(Long achievementId, Long userId) {
+		Achievement achievement = achievementRepository.findById(achievementId).orElseThrow(() -> new AppException(
+			ErrorCode.ACHIEVEMENT_NOT_FOUND));
+		Optional<UserAchievement> userAchievement = userAchievementRepository.findByIdAndUserId(achievementId, userId);
+		if (userAchievement.isPresent()) {
+			return AchievementResponse.from(achievement, userAchievement.get());
+		} else {
+			return AchievementResponse.from(achievement);
+		}
 	}
 }
