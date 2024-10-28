c99e48eb9ba2db357ed53b2a8974f9ed06c5c1d8
Koo Min <koomin1227@naver.com>
2024-10-28 17:34:07 +0900

    M3-413 Feat : UserAchievement 엔티티 추가

---
+++ src/main/java/com/m3pro/groundflip/domain/entity/UserAchievement.java
@@ -0,0 +1,52 @@
+package com.m3pro.groundflip.domain.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class UserAchievement {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "achievement_id")
+	private Achievement achievement;
+
+	@Column(name = "current_value")
+	private Integer currentValue;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Column(name = "modified_at")
+	private LocalDateTime modifiedAt;
+
+	@Column(name = "obtained_at")
+	private LocalDateTime obtainedAt;
+
+	@Column(name = "is_reward_received")
+	private Boolean isRewardReceived;
+
+}
