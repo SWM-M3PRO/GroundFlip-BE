6c27e3d26960c3fdd2a31bc29350f43373cbdc24
Koo Min <koomin1227@naver.com>
2024-11-02 20:33:06 +0900

    M3-418 Feat : user 의 알림 목록을 가져오는 메서드 구현

---
+++ src/main/java/com/m3pro/groundflip/service/NotificationService.java
@@ -0,0 +1,27 @@
+package com.m3pro.groundflip.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.m3pro.groundflip.domain.dto.notification.NotificationResponse;
+import com.m3pro.groundflip.domain.entity.UserNotification;
+import com.m3pro.groundflip.repository.UserNotificationRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+	private final UserNotificationRepository userNotificationRepository;
+
+	public List<NotificationResponse> getAllNotifications(Long userId) {
+		LocalDateTime lookupDate = LocalDateTime.now().minusDays(14);
+		List<UserNotification> userNotifications = userNotificationRepository.findAllByUserId(userId, lookupDate);
+
+		return userNotifications.stream().map((NotificationResponse::from)).toList();
+	}
+}
